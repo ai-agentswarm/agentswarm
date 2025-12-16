05a0a9ae4e791ad64258fde8eeec4b3e883c816d
Luca Roverelli <lucaroverelli@boris.local>
2025-12-16 22:51:32 +0100

    chore(datamodels): updated context with new store definition

--- src/agentswarm/datamodels/__init__.py
+++ src/agentswarm/datamodels/__init__.py
@@ -1,6 +1,8 @@
 from .context import Context
 from .message import Message
 from .responses import Response, KeyStoreResponse, VoidResponse, ThoughtResponse
+from .store import Store
+from .local_store import LocalStore
 
 __all__ = [
     "Context",
@@ -9,4 +11,6 @@ __all__ = [
     "KeyStoreResponse",
     "VoidResponse",
     "ThoughtResponse",
+    "Store",
+    "LocalStore",
 ]
--- src/agentswarm/datamodels/context.py
+++ src/agentswarm/datamodels/context.py
@@ -3,38 +3,76 @@ import uuid
 from typing import List, Optional
 
 from .message import Message
-
+from ..llms import LLMUsage, LLM
+from .store import Store
 
 class Context():
-    trace_id: str
-    step_id: str
-    parent_step_id: Optional[str]
-    messages: List[Message]
-    store: dict
-    thoughts: list[str]
+    """
+    The Context class contains all the information of the current context, with the messages, store, usage and so on.
+    Moreover, the Context contains informations about the tracing and current execution step.
+    """
 
-    def __init__(self, trace_id: str, messages: List[Message], store: dict, thoughts: list[str], step_id: str = None, parent_step_id: str = None):
+    # The trace_id is the unique identifier of the current trace.
+    trace_id: str
+    # The step_id is the unique identifier of the current step, inside the trace
+    step_id: str
+    # The parent_step_id is the unique identifier of the parent step, that originally creates this step
+    parent_step_id: Optional[str]
+    # The list of the messages of the current context
+    messages: List[Message]
+    # Reference to the current store
+    store: Store
+    # List of the thoughts generated in the current context by LLMs
+    thoughts: list[str]
+    # Total (current) usage of the context stack
+    usage: list[LLMUsage]
+
+
+    def __init__(self, trace_id: str, messages: List[Message], store: Store, thoughts: list[str], step_id: str = None, parent_step_id: str = None, default_llm: Optional[LLM] = None):
         self.trace_id = trace_id
         self.step_id = step_id if step_id else str(uuid.uuid4())
         self.parent_step_id = parent_step_id
         self.messages = messages
         self.store = store
         self.thoughts = thoughts
+        self.default_llm = default_llm
 
-    def get_store(self, key: str):
-        if self.store is None:
-            return None
-        return self.store.get(key)
+    def copy_for_execution(self):
+        """
+        Copy the current context for a new (clean) execution.
+        The new context will have a cleaned messages list and thoughts, and will have a new step_id.
+        The parent_step_id of the new context will be the current step_id, in order to trace the execution hierarchy.
 
-    def set_store(self, key: str, value: any):
-        if self.store is None:
-            self.store = {}
-        self.store[key] = value
+        The store and the default_llm will remain the same.
+        """
+        new_context = Context(
+            trace_id=self.trace_id,
+            messages=[],
+            store=self.store,
+            thoughts=[],
+            parent_step_id=self.step_id,
+            default_llm=self.default_llm
+        )
+        return new_context
 
-    def has_store(self, key: str) -> bool:
-        if self.store is None:
-            return False
-        return key in self.store
+    def copy_for_iteration(self, step_id: str, messages: List[Message]):
+        """
+        Copy the current context for a new iteration with the specified step_id and messages.
+        The new context will have the same messages and thoughts.
+        The parent_step_id of the new context will be the current step_id, in order to trace the iteration hierarchy.
+
+        The store and the default_llm will remain the same.
+        """
+        iter_context = Context(
+            trace_id=self.trace_id,
+            messages=messages,
+            store=self.store,
+            thoughts=self.thoughts,
+            step_id=step_id,
+            parent_step_id=self.step_id,
+            default_llm=self.default_llm
+        )
+        return iter_context
 
     def debug_print(self) -> str:
         str_len = 100

e529eec1582d98b899e4db9032fdf9bb99a7147e
Luca Roverelli <lucaroverelli@boris.local>
2026-01-16 17:40:54 +0100

    fix: solved exports

--- src/agentswarm/datamodels/__init__.py
+++ src/agentswarm/datamodels/__init__.py
@@ -1,13 +1,13 @@
 from .context import Context
 from .message import Message
-from .responses import Response, KeyStoreResponse, VoidResponse, ThoughtResponse
+from .responses import KeyStoreResponse, VoidResponse, ThoughtResponse, StrResponse
 from .store import Store
 from .local_store import LocalStore
 
 __all__ = [
     "Context",
     "Message",
-    "Response",
+    "StrResponse",
     "KeyStoreResponse",
     "VoidResponse",
     "ThoughtResponse",
